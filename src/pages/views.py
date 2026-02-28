65db7af04e66cf864d42e02d206f77e1e26032a7
Ali Vega <ali.vega93@hotmail.com>
2026-02-27 22:56:53 -0600

    actividad django

--- src/pages/views.py
+++ src/pages/views.py
@@ -4,12 +4,24 @@ from django import get_version
 from django.conf import settings
 from django.shortcuts import render
 
+class Proyecto:
+    def __init__(self, nombre, autora):
+        self.nombre = nombre
+        self.autora = autora
 
 def home(request):
+
+    proyecto = Proyecto(
+        nombre="Fundamentos de Linux e Introducción a Django",
+        autora="Ali Vega"
+    )
+
     context = {
         "debug": settings.DEBUG,
-        "django_ver": get_version(),
-        "python_ver": os.environ["PYTHON_VERSION"],
+        "django_ver": get_version() + " PROBANDO CAMBIOS",
+        "python_ver": os.environ["PYTHON_VERSION"] + " MAS CAMBIOS",        
+        "project_name": proyecto.nombre,
+        "project_author": proyecto.autora,
     }
 
     return render(request, "pages/home.html", context)
